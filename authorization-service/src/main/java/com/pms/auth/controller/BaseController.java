6534c9eeefb75a02ffe8ecf361688b205d9037a0
TarunPrajapat <42228893+TarunPrajapat@users.noreply.github.com>
2022-08-18 15:39:37 +0530

    Add files via upload

---
+++ authorization-service/src/main/java/com/pms/auth/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.pms.auth.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class BaseController {
+
+}
